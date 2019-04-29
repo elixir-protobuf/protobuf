a8ff06d0a13d7e72b5db7556cf667da8fdc80a4c
matehat <mathieu@damours.org>
2019-04-29 15:05:23 -0400

    Add type_url/0 to Protobuf generated modules

--- lib/protobuf.ex
+++ lib/protobuf.ex
@@ -1,5 +1,17 @@
 defmodule Protobuf do
   defmacro __using__(opts) do
+    path = __CALLER__.module
+      |> Module.split()
+      |> Enum.reverse()
+      |> Enum.with_index()
+      |> Enum.map(fn
+        {item, index} when index > 0 -> String.downcase(item)
+        {item, _} -> item
+      end)
+      |> Enum.reverse()
+      |> Enum.join(".")
+    type_url = "type.googleapis.com/" <> path
+
     quote do
       import Protobuf.DSL, only: [field: 3, field: 2, oneof: 2]
       Module.register_attribute(__MODULE__, :fields, accumulate: true)
@@ -15,6 +27,10 @@ defmodule Protobuf do
 
       def new(attrs) do
         Protobuf.Builder.new(__MODULE__, attrs)
+      end
+
+      def type_url() do
+        unquote(type_url)
       end
     end
   end
