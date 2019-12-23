35a0e2ae8d92e329174b0efa5a2d6899415d47d8
Pawel Antemijczuk <pawel.antemijczuk1@motorolasolutions.com>
2019-12-23 11:46:05 +0100

    Change instrumentation

--- test/protobuf/decoder_test.exs
+++ test/protobuf/decoder_test.exs
@@ -29,7 +29,7 @@ defmodule Protobuf.DecoderTest do
   end
 
   test "raises for bad binaries" do
-    assert_raise(Protobuf.DecodeError, ~r{cannot decode binary data as}, fn ->
+    assert_raise(Protobuf.DecodeError, ~r{cannot decode binary data}, fn ->
       Decoder.decode(<<0, 0, 0, 0, 0, 0, 0>>, TestMsg.Foo)
     end)
   end
