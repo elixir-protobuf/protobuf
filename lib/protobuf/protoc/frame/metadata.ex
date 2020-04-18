09e04c147a798f7643f64557b06c22759daf94fd
csWen <707977597@qq.com>
2020-04-18 13:16:05 +0800

    Add nested messages parse.

--- lib/protobuf/protoc/frame/metadata.ex
+++ lib/protobuf/protoc/frame/metadata.ex
@@ -3,29 +3,92 @@ defmodule Protobuf.Protoc.Metadata do
   Most of functions read data from ETS.
   """
 
+  @spec get_desc_by_fqn(String.t()) :: map() | nil
+  def get_desc_by_fqn(fqn) do
+    [{{:desc, ^fqn}, desc}] = :ets.lookup(Protobuf.Protoc.Parser, {:desc, fqn})
+    desc
+  end
+
+  @spec get_metadata_by_fqn(String.t()) :: map() | nil
+  def get_metadata_by_fqn(fqn) do
+    [{{:metadata, ^fqn}, metadata}] = :ets.lookup(Protobuf.Protoc.Parser, {:metadata, fqn})
+    metadata
+  end
+
   defmodule Package do
     @moduledoc ""
 
+    @type t :: %__MODULE__{
+            fqn: String.t(),
+            files: [map()]
+          }
+
     defstruct fqn: "", files: []
 
-    def files(_pkg) do
+    alias Protobuf.Protoc.Metadata
+
+    @spec files([__MODULE__.t()]) :: [
+            {Metadata.File.t(), Google.Protobuf.FileDescriptorProto.t()}
+          ]
+    def files(pkgs) when is_list(pkgs) do
+      Enum.flat_map(pkgs, &files/1)
+    end
+
+    @spec files(__MODULE__.t()) :: [{Metadata.File.t(), Google.Protobuf.FileDescriptorProto.t()}]
+    def files(pkg) do
+      pkg
+      |> Map.get(:files)
+      |> Enum.map(fn %{fqn: fqn} ->
+        {Metadata.get_metadata_by_fqn(fqn), Metadata.get_desc_by_fqn(fqn)}
+      end)
     end
   end
 
   defmodule File do
     @moduledoc ""
+
+    @type t :: %__MODULE__{
+            desc: map() | nil,
+            fqn: String.t(),
+            package: String.t(),
+            messages: [String.t()]
+          }
+
     defstruct desc: nil, fqn: "", package: "", messages: []
+
+    alias Protobuf.Protoc.Metadata
 
     @doc """
     Read all direct messages and nested messages
     """
-    def all_messages(_file) do
+    @spec all_messages(__MODULE__.t()) :: [
+            {Metadata.Message.t(), Google.Protobuf.DescriptorProto.t()}
+          ]
+    def all_messages(file) do
+      file
+      |> Map.get(:messages)
+      |> Enum.flat_map(fn message_fqn ->
+        msg_desc = Metadata.get_desc_by_fqn(message_fqn)
+        msg_md = Metadata.get_metadata_by_fqn(message_fqn)
+        [{msg_md, msg_desc} | Metadata.Message.all_messages(msg_md)]
+      end)
     end
 
     @doc """
     Read direct messages
     """
-    def messages(_file) do
+    @spec messages(__MODULE__.t()) :: [
+            {Metadata.Message.t(), Google.Protobuf.DescriptorProto.t()}
+          ]
+    def messages(file) do
+      file
+      |> Map.get(:messages)
+      |> Enum.map(fn msg_fqn ->
+        {
+          Protobuf.Protoc.Metadata.get_metadata_by_fqn(msg_fqn),
+          Protobuf.Protoc.Metadata.get_desc_by_fqn(msg_fqn)
+        }
+      end)
     end
 
     @doc """
@@ -43,12 +106,39 @@ defmodule Protobuf.Protoc.Metadata do
 
   defmodule Message do
     @moduledoc ""
-    defstruct desc: nil, fqn: "", namespace: "", file: "", parent_msg: "", fields: []
+
+    @type t :: %__MODULE__{
+            desc: map() | nil,
+            fqn: String.t(),
+            namespace: String.t(),
+            file: String.t(),
+            parent_msg: String.t(),
+            fields: [String.t()],
+            nested_msgs: [String.t()]
+          }
+
+    defstruct desc: nil,
+              fqn: "",
+              namespace: "",
+              file: "",
+              parent_msg: "",
+              fields: [],
+              nested_msgs: []
+
+    alias Protobuf.Protoc.Metadata
 
     @doc """
     Return all nested messages
     """
-    def all_messages(_msg) do
+    @spec all_messages(__MODULE__.t()) :: [{__MODULE__.t(), Google.Protobuf.DescriptorProto.t()}]
+    def all_messages(msg) do
+      msg
+      |> Map.get(:nested_msgs)
+      |> Enum.flat_map(fn msg_fqn ->
+        msg_desc = Metadata.get_desc_by_fqn(msg_fqn)
+        msg_md = Metadata.get_metadata_by_fqn(msg_fqn)
+        [{msg_md, msg_desc} | all_messages(msg_md)]
+      end)
     end
 
     @doc """
@@ -61,11 +151,33 @@ defmodule Protobuf.Protoc.Metadata do
     Read all direct enums
     """
     def enums(_msg) do
+      []
+    end
+
+    @spec fields(__MODULE__.t()) :: [
+            {Metadata.Field.t(), Google.Protobuf.FieldDescriptorProto.t()}
+          ]
+    def fields(msg) do
+      msg
+      |> Map.get(:fields)
+      |> Enum.map(fn field_fqn ->
+        {
+          Metadata.get_metadata_by_fqn(field_fqn),
+          Metadata.get_desc_by_fqn(field_fqn)
+        }
+      end)
     end
   end
 
   defmodule Field do
     @moduledoc ""
+
+    @type t :: %__MODULE__{
+            desc: map() | nil,
+            fqn: String.t(),
+            message: String.t()
+          }
+
     defstruct desc: nil, fqn: "", message: ""
   end
 end
