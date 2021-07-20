tony612/protobuf-elixir

Refactor start/2 and get_all_modules/0
to concentrate module loading logic.

lib/application.ex
@@ -10,29 +10,29 @@ defmodule Protobuf.Application do
 
   @doc false
   def start(_type, _args) do
-    if Application.get_env(:protobuf, :extensions, :disabled) == :enabled do
-      mods = get_all_modules()
-      Protobuf.Extension.cal_extensions(mods)
-    else
-      # Extensions in Protobuf should always be calculated for generating code
-      {:ok, mods} = :application.get_key(:protobuf, :modules)
-      Protobuf.Extension.cal_extensions(mods)
-    end
+    Protobuf.Extension.cal_extensions(get_all_modules())
 
-    children = []
-    Supervisor.start_link(children, strategy: :one_for_one)
+    Supervisor.start_link(_children = [], strategy: :one_for_one)
   end
 
-  defp get_all_modules() do
-    case :code.get_mode() do
-      :embedded ->
-        :erlang.loaded()
+  defp get_all_modules do
+    case Application.get_env(:protobuf, :extensions) do
+      :enabled ->
+        case :code.get_mode() do
+          :embedded ->
+            :erlang.loaded()
 
-      :interactive ->
-        Enum.flat_map(Application.loaded_applications(), fn {app, _desc, _vsn} ->
-          {:ok, modules} = :application.get_key(app, :modules)
-          modules
-        end)
+          :interactive ->
+            Enum.flat_map(Application.loaded_applications(), fn {app, _desc, _vsn} ->
+              {:ok, modules} = :application.get_key(app, :modules)
+              modules
+            end)
+        end
+
+      _disabled ->
+        # Extensions in Protobuf are required for generating code
+        {:ok, mods} = :application.get_key(:protobuf, :modules)
+        mods
     end
   end
 end

elixir-protobuf/protobuf

lib/application.ex
@@ -11,35 +11,36 @@ defmodule Protobuf.Application do
   @doc false
   @impl true
   def start(_type, _args) do
-    if Application.get_env(:protobuf, :extensions, :disabled) == :enabled do
-      mods = get_all_modules()
-      Protobuf.Extension.__cal_extensions__(mods)
-    else
-      # Extensions in Protobuf should always be calculated for generating code
-      {:ok, mods} = :application.get_key(:protobuf, :modules)
-      Protobuf.Extension.__cal_extensions__(mods)
-    end
+    Protobuf.Extension.__cal_extensions__(get_all_modules())
 
-    children = []
-    Supervisor.start_link(children, strategy: :one_for_one)
+    Supervisor.start_link(_children = [], strategy: :one_for_one)
   end
 
+  @doc false
   @impl true
   def stop(_state) do
     Protobuf.Extension.__unload_extensions__()
     :ok
   end
 
-  defp get_all_modules() do
-    case :code.get_mode() do
-      :embedded ->
-        :erlang.loaded()
+  defp get_all_modules do
+    case Application.get_env(:protobuf, :extensions) do
+      :enabled ->
+        case :code.get_mode() do
+          :embedded ->
+            :erlang.loaded()
 
-      :interactive ->
-        Enum.flat_map(Application.loaded_applications(), fn {app, _desc, _vsn} ->
-          {:ok, modules} = :application.get_key(app, :modules)
-          modules
-        end)
+          :interactive ->
+            Enum.flat_map(Application.loaded_applications(), fn {app, _desc, _vsn} ->
+              {:ok, modules} = :application.get_key(app, :modules)
+              modules
+            end)
+        end
+
+      _disabled ->
+        # Extensions in Protobuf are required for generating code
+        {:ok, mods} = :application.get_key(:protobuf, :modules)
+        mods
     end
   end
 end