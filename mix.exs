f84df5c9f9495c47b1e1efb9ec7c55b66bb32e31
taotao.lin <cnredink@gmail.com>
2021-08-16 12:27:09 +0800

    [update] update ci version and deps version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Protobuf.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/elixir-protobuf/protobuf"
-  @version "0.8.0-beta.1"
+  @version "0.8.1"
 
   def project do
     [
