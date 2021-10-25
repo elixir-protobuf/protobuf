3e15cc1c454fba34539570e4c7584de4fd4157b2
João Britto <jabcalves@gmail.com>
2021-10-25 03:03:17 -0300

    Add repeated scalar types to property-based roundtrip tests (#172)

--- test/pbt/encode_decode_type_test.exs
+++ test/pbt/encode_decode_type_test.exs
@@ -11,7 +11,7 @@ defmodule Protobuf.EncodeDecodeTypeTest.PropertyGenerator do
     |> Protobuf.Encoder.encode(iolist: false)
   end
 
-  defmacro make_property(gen_func, field_type) do
+  defmacro make_properties(gen_func, field_type) do
     quote do
       property "#{unquote(field_type)} roundtrip" do
         check all n <- unquote(gen_func) do
@@ -21,13 +21,22 @@ defmodule Protobuf.EncodeDecodeTypeTest.PropertyGenerator do
           assert n == decode(field_type, bin)
         end
       end
+
+      property "repeated #{unquote(field_type)} roundtrip" do
+        check all n <- unquote(gen_func) do
+          field_type = :"repeated_#{unquote(field_type)}"
+          bin = encode(field_type, [n, n])
+
+          assert [n, n] == decode(field_type, bin)
+        end
+      end
     end
   end
 
   # Since float point is not precise, make canonical value before doing PBT
   # ref: http://hypothesis.works/articles/canonical-serialization/
   # and try 0.2 here: https://www.h-schmidt.net/FloatConverter/IEEE754.html
-  defmacro make_canonical_property(gen_func, field_type) do
+  defmacro make_canonical_properties(gen_func, field_type) do
     quote do
       property "#{unquote(field_type)} canonical roundtrip" do
         check all n <- unquote(gen_func) do
@@ -37,6 +46,17 @@ defmodule Protobuf.EncodeDecodeTypeTest.PropertyGenerator do
           bin = encode(field_type, canonical_val)
 
           assert canonical_val == decode(field_type, bin)
+        end
+      end
+
+      property "repeated #{unquote(field_type)} canonical roundtrip" do
+        check all n <- unquote(gen_func) do
+          field_type = :"repeated_#{unquote(field_type)}"
+          encoded_vals = encode(field_type, [n, n])
+          canonical_vals = decode(field_type, encoded_vals)
+          bin = encode(field_type, canonical_vals)
+
+          assert canonical_vals == decode(field_type, bin)
         end
       end
     end
@@ -65,18 +85,18 @@ defmodule Protobuf.EncodeDecodeTypeTest do
     map(integer(), &abs/1)
   end
 
-  make_property(integer(), :int32)
-  make_property(large_integer(), :int64)
-  make_property(uint32_gen(), :uint32)
-  make_property(uint64_gen(), :uint64)
-  make_property(integer(), :sint32)
-  make_property(large_integer(), :sint64)
+  make_properties(integer(), :int32)
+  make_properties(large_integer(), :int64)
+  make_properties(uint32_gen(), :uint32)
+  make_properties(uint64_gen(), :uint64)
+  make_properties(integer(), :sint32)
+  make_properties(large_integer(), :sint64)
 
-  make_property(boolean(), :bool)
+  make_properties(boolean(), :bool)
 
-  make_property(natural_number(), :fixed64)
-  make_property(large_integer(), :sfixed64)
+  make_properties(natural_number(), :fixed64)
+  make_properties(large_integer(), :sfixed64)
 
-  make_canonical_property(float(), :double)
-  make_canonical_property(float(), :float)
+  make_canonical_properties(float(), :double)
+  make_canonical_properties(float(), :float)
 end
