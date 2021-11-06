97a8b4843715cbecce300fd9016cf0293ef3890b
Andrea Leopardi <an.leopardi@gmail.com>
2021-11-06 17:11:52 +0100

    Improve enum generation tests

--- test/protobuf/protoc/generator/enum_test.exs
+++ test/protobuf/protoc/generator/enum_test.exs
@@ -6,9 +6,10 @@ defmodule Protobuf.Protoc.Generator.EnumTest do
 
   test "generate/2 generates enum type messages" do
     ctx = %Context{package: ""}
+    module = Module.concat(__MODULE__, "EnumFoo") |> inspect() |> String.replace(".", "")
 
     desc = %Google.Protobuf.EnumDescriptorProto{
-      name: "EnumFoo",
+      name: module,
       options: nil,
       value: [
         Google.Protobuf.EnumValueDescriptorProto.new(name: "A", number: 0),
@@ -20,7 +21,12 @@ defmodule Protobuf.Protoc.Generator.EnumTest do
     }
 
     msg = Generator.generate(ctx, desc)
-    assert msg =~ "defmodule EnumFoo do\n"
+
+    # Make sure the generated file is compilable.
+    assert [{compiled_mod, _bytecode}] = Code.compile_string(msg)
+    assert inspect(compiled_mod) == module
+
+    assert msg =~ "defmodule #{module} do\n"
     assert msg =~ "use Protobuf, enum: true\n"
 
     assert msg =~
@@ -34,6 +40,53 @@ defmodule Protobuf.Protoc.Generator.EnumTest do
              field :HAS_UNDERSCORES, 2
              field :HAS_UNDERSCORES_X, 3
              field :HAS_UNDERSCORES_, 4
+           """
+  end
+
+  test "generate/2 generates enum type messages with descriptor" do
+    ctx = %Context{package: "", gen_descriptors?: true}
+    module = Module.concat(__MODULE__, "EnumFooDesc") |> inspect() |> String.replace(".", "")
+
+    desc = %Google.Protobuf.EnumDescriptorProto{
+      name: module,
+      options: nil,
+      value: [
+        Google.Protobuf.EnumValueDescriptorProto.new(name: "A", number: 0),
+        Google.Protobuf.EnumValueDescriptorProto.new(name: "B", number: 1),
+        Google.Protobuf.EnumValueDescriptorProto.new(name: "HAS_UNDERSCORES", number: 2),
+        Google.Protobuf.EnumValueDescriptorProto.new(name: "HAS_UNDERSCORES_X", number: 3),
+        Google.Protobuf.EnumValueDescriptorProto.new(name: "HAS_UNDERSCORES_", number: 4)
+      ]
+    }
+
+    msg = Generator.generate(ctx, desc)
+
+    # Make sure the generated file is compilable.
+    assert [{compiled_mod, _bytecode}] = Code.compile_string(msg)
+    assert inspect(compiled_mod) == module
+
+    assert msg =~ "defmodule #{module} do\n"
+    assert msg =~ "use Protobuf, enum: true\n"
+
+    assert msg =~
+             "@type t :: integer | :A | :B | :HAS_UNDERSCORES | :HAS_UNDERSCORES_X | :HAS_UNDERSCORES_\n"
+
+    refute msg =~ "defstruct "
+
+    assert msg =~ """
+             field :A, 0
+             field :B, 1
+             field :HAS_UNDERSCORES, 2
+             field :HAS_UNDERSCORES_X, 3
+             field :HAS_UNDERSCORES_, 4
+           """
+
+    assert %Google.Protobuf.EnumDescriptorProto{} = desc = compiled_mod.descriptor
+    assert desc.name == module
+
+    assert msg =~ """
+             def descriptor do
+               # credo:disable-for-next-line
            """
   end
 end
