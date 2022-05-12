dd77bddf066bd4365236179a74e6bac7fa934de7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2022-05-12 18:24:12 +0200

    Release v0.10.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Protobuf.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/elixir-protobuf/protobuf"
-  @version "0.9.0-dev"
+  @version "0.10.0"
   @description "A pure Elixir implementation of Google Protobuf."
 
   def project do
