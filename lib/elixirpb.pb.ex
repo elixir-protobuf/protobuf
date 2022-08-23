df9335b6731720cf049b59d2e05912fc66d76c9a
antedeguemon <antedeguemon@users.noreply.github.com>
2022-08-23 11:54:15 -0400

    Update generated protobufs

--- lib/elixirpb.pb.ex
+++ lib/elixirpb.pb.ex
@@ -1,5 +1,6 @@
 defmodule Elixirpb.FileOptions do
   @moduledoc false
+
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto2
 
   field :module_prefix, 1, optional: true, type: :string
