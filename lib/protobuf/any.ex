e34f9f4da7a8257423dceded12a32b86b635a430
Andrea leopardi <an.leopardi@gmail.com>
2022-12-12 20:02:14 +0100

    Improve docs for Protobuf.Any

--- lib/protobuf/any.ex
+++ lib/protobuf/any.ex
@@ -1,14 +1,19 @@
 defmodule Protobuf.Any do
   @moduledoc """
-  Provides functions for working with the `google.protobuf.Any` well known type.
+  Provides functions for working with the `google.protobuf.Any` type.
 
-  Using the Any type provides the ability to send any message type in your messages (a polymorphic field).
+  Using the `Any` type provides the ability to send any message type in your messages, effectively
+  providing a polymorphic field.
 
-  See: [Documentation for the Any type](https://developers.google.com/protocol-buffers/docs/proto3#any)
+  See [Google's documentation for the `Any`a
+  type](https://developers.google.com/protocol-buffers/docs/proto3#any).
 
-  Utilizing these functions allows you to pack and unpack any fields with any protocol buffer message.any()
+  Utilizing these functions allows you to pack and unpack any fields with any Protobuf
+  message.
 
-  Given a message type of
+  ## Examples
+
+  Imagine you have this Protobuf schema:
 
   ```protobuf
   message ErrorStatus {
@@ -17,34 +22,46 @@ defmodule Protobuf.Any do
   }
   ```
 
-  Use the `Protobuf.Any.pack` function to set the information.
+  You can use `pack/1` to pack any message into an `Any` message.
 
-  ```elixir
-  details = some_list_of_protobuf_messages()
-  ErrorStatus.new(%{
-    message: "There was an error",
-    details: Enum.map(details, &Protobuf.Any.pack/1)
-  })
-  ```
+      details = some_list_of_protobuf_messages()
+
+      ErrorStatus.new(%{
+        message: "There was an error",
+        details: Enum.map(details, &Protobuf.Any.pack/1)
+      })
 
   """
+
   @type_url_prefix "type.googleapis.com/"
-  @type option :: {:prefix, module()}
+  @type option() :: {:prefix, module()}
 
   @doc """
-  Pack a protocol buffer message into a `Google.Protobuf.Any` message.
+  Packs a Protobuf message into a `Google.Protobuf.Any` message.
 
-  This sets the correct `type_url` of the pattern: `type.googleapis.com/<package>.<message name>`
-  and the `value` of the `Any` as the serialized original message.
+  This sets the correct `type_url` using the pattern:
+
+      type.googleapis.com/<package>.<message name>
+
+  This URL is obtained by calling the `fully_qualified_name/1` function
+  on the module for the `data` struct.
+
+  The `value` field of the `Google.Protobuf.Any` returned message is set to
+  the serialized original message.
 
   ## Example
 
-  ```elixir
-  Google.Protobuf.Any.new(%{
-    type_url: "type.googleapis.com/some.package.My.Message",
-    value: Some.Package.My.Message.encode(data)
-  }) = Protobuf.Any.pack(data)
-  ```
+      # This is the arbitrary message we want to pack.
+      encoded = MyPkg.MyMessage.encode(data)
+
+      any = Google.Protobuf.Any.new(%{
+        type_url: "type.googleapis.com/my_pkg.MyMessage",
+        value: encoded
+      })
+
+      any == Protobuf.Any.pack(data)
+      #=> true
+
   """
   @spec pack(struct()) :: Google.Protobuf.Any.t()
   def pack(%mod{} = data) do
@@ -55,46 +72,53 @@ defmodule Protobuf.Any do
   end
 
   @doc """
-  Unpack a `Google.Protobuf.Any` message.
+  Unpacks a `Google.Protobuf.Any` message.
 
-  Utilizes the `type_url` to determine the type, and deserializes the binary data into that type.
+  Uses the `type_url` to determine the type, and deserializes the binary data into that type.
+
+  > #### Existing modules {: .warning}
+  >
+  > The module for the message is determined via a **string** (the `type_url`),
+  > which is arbitrary user input that maps the package message name
+  > to a generated protocol buffer module.
+  >
+  > Because of this, this function uses `Module.safe_concat/1` is used to prevent arbitrary
+  > atom creation, which _requires_ that the message type is known and compiled into the
+  > application.
+  > If the inferred type is unknown, this function will raise an error.
+
+  ## Options
+
+  If you generated the protocol buffers with a module prefix, you can use
+  the `:prefix` option (a module) to unpack this correctly.
 
   ## Example
 
-      any = %Google.Protobuf.Any{type_url: "type.googleapis.com/some.package.My.Message", value: <binary_data>}
-      %Some.Package.My.Message{} = Protobuf.Any.unpack(any)
+      any = %Google.Protobuf.Any{
+        type_url: "type.googleapis.com/my_pkg.MyMessage",
+        value: <<...>>
+      }
 
-  Note: Since the module for the message is determined via a string (type_url) that is user input that maps the package message name
-  to a generated protocol buffer module. `Module.safe_concat` is used to prevent arbitrary atom creation, which _requires_ that the message type is known and compiled into the application.
-  If the inferred type is unknown there will be an Argument error raised.
-
-  If you generated the protocol buffers with a module prefix, you can use the `prefix` option to unpack this correctly.
-
-  ## Example
-      any = %Google.Protobuf.Any{type_url: "type.googleapis.com/some.package.My.Message", value: <binary_data>}
-      %Prefixed.Some.Package.My.Message{} = Protobuf.Any.unpack(any, prefix: Prefixed)
+      Protobuf.Any.unpack(any)
+      #=> %MyPkg.MyMessage{...}
 
   """
-  @spec unpack(Google.Protobuf.Any.t()) :: struct()
-  @spec unpack(Google.Protobuf.Any.t(), [option()]) :: struct()
+  @spec unpack(Google.Protobuf.Any.t(), keyword()) :: struct()
   def unpack(
         %{__struct__: Google.Protobuf.Any, type_url: @type_url_prefix <> name, value: value},
         options \\ []
       ) do
-    prefix = Keyword.get(options, :prefix, nil)
-
     parts =
       name
       |> String.split(".")
       |> Enum.map(&Macro.camelize/1)
 
-    mod =
-      if prefix do
-        Module.safe_concat([prefix] ++ parts)
-      else
-        Module.safe_concat(parts)
+    parts =
+      case Keyword.fetch(options, :prefix) do
+        {:ok, prefix} -> [prefix] ++ parts
+        :error -> parts
       end
 
-    mod.decode(value)
+    Module.safe_concat(parts).decode(value)
   end
 end
--- test/protobuf/any_test.exs
+++ test/protobuf/any_test.exs
@@ -1,39 +1,43 @@
 defmodule Protobuf.AnyTest do
   use ExUnit.Case, async: true
 
-  test "packs a message into an Any protobuf" do
-    message = Google.Protobuf.Duration.new(%{seconds: 42})
-    assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
-    assert result.type_url == "type.googleapis.com/google.protobuf.Duration"
-    assert result.value == Google.Protobuf.Duration.encode(message)
+  describe "pack/1" do
+    test "packs a message into Any" do
+      message = Google.Protobuf.Duration.new(seconds: 42)
+      assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
+      assert result.type_url == "type.googleapis.com/google.protobuf.Duration"
+      assert result.value == Google.Protobuf.Duration.encode(message)
+    end
+
+    test "packs a message into Any with a prefix" do
+      message = My.Test.Request.SomeGroup.new(group_field: 42)
+      assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
+      assert result.type_url == "type.googleapis.com/test.Request.SomeGroup"
+      assert result.value == My.Test.Request.SomeGroup.encode(message)
+    end
   end
 
-  test "packs a message into an Any protobuf with a prefix" do
-    message = My.Test.Request.SomeGroup.new(%{group_field: 42})
-    assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
-    assert result.type_url == "type.googleapis.com/test.Request.SomeGroup"
-    assert result.value == My.Test.Request.SomeGroup.encode(message)
-  end
+  describe "unpack/2" do
+    test "unpacks a message into Any" do
+      message = Google.Protobuf.Duration.new(seconds: 42)
 
-  test "unpacks a message into an Any protobuf" do
-    message = Google.Protobuf.Duration.new(%{seconds: 42})
+      any = %Google.Protobuf.Any{
+        type_url: "type.googleapis.com/google.protobuf.Duration",
+        value: Protobuf.encode(message)
+      }
 
-    any = %Google.Protobuf.Any{
-      type_url: "type.googleapis.com/google.protobuf.Duration",
-      value: Protobuf.encode(message)
-    }
+      assert ^message = Protobuf.Any.unpack(any)
+    end
 
-    assert ^message = Protobuf.Any.unpack(any)
-  end
+    test "unpacks a message into Any with a prefix" do
+      message = My.Test.Request.SomeGroup.new(group_field: 42)
 
-  test "unpacks a message into an Any protobuf with a prefix" do
-    message = My.Test.Request.SomeGroup.new(%{group_field: 42})
+      any = %Google.Protobuf.Any{
+        type_url: "type.googleapis.com/test.Request.SomeGroup",
+        value: Protobuf.encode(message)
+      }
 
-    any = %Google.Protobuf.Any{
-      type_url: "type.googleapis.com/test.Request.SomeGroup",
-      value: Protobuf.encode(message)
-    }
-
-    assert ^message = Protobuf.Any.unpack(any, prefix: My)
+      assert ^message = Protobuf.Any.unpack(any, prefix: My)
+    end
   end
 end
