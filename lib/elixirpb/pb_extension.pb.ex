2934487154d5fbe56ebcc4c55cd9d8d7a5c30934
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2023-04-25 20:41:30 +0200

    Release v0.12.0

--- lib/elixirpb/pb_extension.pb.ex
+++ lib/elixirpb/pb_extension.pb.ex
@@ -1,6 +1,6 @@
 defmodule Elixirpb.PbExtension do
   @moduledoc false
-  use Protobuf, protoc_gen_elixir_version: "0.11.0"
+  use Protobuf, protoc_gen_elixir_version: "0.12.0"
 
   extend Google.Protobuf.FileOptions, :file, 1047, optional: true, type: Elixirpb.FileOptions
 end
