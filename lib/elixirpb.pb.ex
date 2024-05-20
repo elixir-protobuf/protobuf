1dff9295eb8112761f5a5547f7717e144401226e
v0idpwn <v0idpwn@gmail.com>
2024-05-20 19:35:25 -0300

    Generate code from protos with OTP 26
    Changes in ordering make the generated files look different, causing
    a CI error.

--- lib/elixirpb.pb.ex
+++ lib/elixirpb.pb.ex
@@ -1,7 +1,7 @@
 defmodule Elixirpb.FileOptions do
   @moduledoc false
 
-  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto2
+  use Protobuf, syntax: :proto2, protoc_gen_elixir_version: "0.12.0"
 
   field :module_prefix, 1, optional: true, type: :string
 end
