3d0d7b5c96caab03f1b66f85edad596ae6c7b744
v0idpwn <v0idpwn@gmail.com>
2024-06-17 10:56:07 -0300

    Revert "Update stream_data to avoid warnings in tests"
    This reverts commit c8eed16acb3bac765c647c6f0937b6fa70ece820.

--- mix.exs
+++ mix.exs
@@ -49,7 +49,7 @@ defmodule Protobuf.Mixfile do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:stream_data, "~> 1.1", only: [:dev, :test]},
+      {:stream_data, "~> 0.5.0", only: [:dev, :test]},
       {:excoveralls, "~> 0.14.4", only: :test},
 
       # We use this as a dependency because we're sneaky. It's not a Mix dependency at all,
