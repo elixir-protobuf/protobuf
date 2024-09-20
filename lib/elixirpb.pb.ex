df12118d5fa11e9ee104af16f15e88fa925f5ce7
Eric Meadows-Jönsson <eric.meadows.jonsson@gmail.com>
2024-09-20 13:16:04 +0200

    Release v0.13.0 (#376)

--- lib/elixirpb.pb.ex
+++ lib/elixirpb.pb.ex
@@ -1,7 +1,7 @@
 defmodule Elixirpb.FileOptions do
   @moduledoc false
 
-  use Protobuf, syntax: :proto2, protoc_gen_elixir_version: "0.12.0"
+  use Protobuf, syntax: :proto2, protoc_gen_elixir_version: "0.13.0"
 
   field :module_prefix, 1, optional: true, type: :string
 end
