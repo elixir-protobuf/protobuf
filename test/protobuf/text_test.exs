a09c9456449399077f1fb1387650b879f98dbd26
v0idpwn <v0idpwn@gmail.com>
2025-04-10 12:48:12 -0300

    Run conformance for text, fixes for conformance, add unknown fields support
    Most of the conformance tests in the text suite are for decoding, and
    some of them are for unknown fields encoding rules, so we only got value
    out of five of them. Still, they led to significant improvements in the
    implementation.
    
    For one, I figured that the "outer message" **can't** use brackets ("{",
    "}"). I thought they were optional and just not used in the examples. So
    we went from:
    
    ```elixir
    Protobuf.Text.encode(%MyMessage{a: 1, b: 2})
    ```
    
    To:
    ```elixir
    Protobuf.Text.encode(%MyMessage{a: 1, b: 2})
    ```
    
    I also had some trouble trying to align the new version with previous `,`
    behaviour, but thankfully, we are still compatible even with the
    difference (which doesn't look that bad IMO).
    
    I also added unknown fields support, but wasn't able to pass all the
    unknown fields tests, so I skipped them for now. The current version
    does the job of printing them reasonably well, so I'm not too bothered
    with it.

--- test/protobuf/text_test.exs
+++ test/protobuf/text_test.exs
@@ -5,54 +5,52 @@ defmodule Protobuf.TextTest do
 
   test "default fields aren't encoded" do
     # proto3
-    assert "{}" == Text.encode(%TestMsg.Foo{})
+    assert "" == Text.encode(%TestMsg.Foo{})
 
     # proto2
-    assert "{a: 1}" == Text.encode(%TestMsg.Foo2{a: 1})
+    assert "a: 1" == Text.encode(%TestMsg.Foo2{a: 1})
   end
 
   test "encoding basic types" do
-    assert ~S({a: 1, b: "foo"}) == Text.encode(%TestMsg.Foo.Bar{a: 1, b: "foo"})
+    assert ~S(a: 1, b: "foo") == Text.encode(%TestMsg.Foo.Bar{a: 1, b: "foo"})
   end
 
   test "encoding enums" do
-    assert ~S({j: D}) == Text.encode(%TestMsg.Foo{j: :D})
+    assert ~S(j: D) == Text.encode(%TestMsg.Foo{j: :D})
   end
 
   test "encoding repeated" do
-    assert ~S({g: [1, 2, 3, 4, 5, 6]}) == Text.encode(%TestMsg.Foo{g: [1, 2, 3, 4, 5, 6]})
+    assert ~S(g: [1, 2, 3, 4, 5, 6]) == Text.encode(%TestMsg.Foo{g: [1, 2, 3, 4, 5, 6]})
   end
 
   test "encoding large repeated breaks lines" do
     result = Text.encode(%TestMsg.Foo{a: 1, g: List.duplicate(1_111_111_111, 7), j: :D})
 
     assert result == """
-           {
-             a: 1,
-             g: [
-               1111111111,
-               1111111111,
-               1111111111,
-               1111111111,
-               1111111111,
-               1111111111,
-               1111111111
-             ],
-             j: D
-           }\
+           a: 1
+           g: [
+             1111111111,
+             1111111111,
+             1111111111,
+             1111111111,
+             1111111111,
+             1111111111,
+             1111111111
+           ]
+           j: D\
            """
   end
 
   test "encoding nested structs" do
     result = Text.encode(%TestMsg.Foo{e: nil})
-    assert result == ~S({})
+    assert result == ~S()
 
     result = Text.encode(%TestMsg.Foo{e: %TestMsg.Foo.Bar{}})
-    assert result == ~S({e: {}})
+    assert result == ~S(e: {})
 
     result = Text.encode(%TestMsg.Foo{e: %TestMsg.Foo.Bar{a: 1, b: "Hello"}})
 
-    assert result == ~S({e: {a: 1, b: "Hello"}})
+    assert result == ~S(e: {a: 1, b: "Hello"})
   end
 
   test "encoding large nested structs" do
@@ -68,21 +66,19 @@ defmodule Protobuf.TextTest do
       })
 
     assert result == """
-           {
+           a: 1
+           e: {
              a: 1,
-             e: {
+             b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
+           }
+           h: [
+             {a: 5},
+             {
                a: 1,
                b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
              },
-             h: [
-               {a: 5},
-               {
-                 a: 1,
-                 b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
-               },
-               {a: 7}
-             ]
-           }\
+             {a: 7}
+           ]\
            """
   end
 
@@ -100,7 +96,6 @@ defmodule Protobuf.TextTest do
     result_with_large_limit = Text.encode(input, max_line_width: 1000)
 
     assert result_with_large_limit == """
-           {\
            a: 1, \
            e: {\
            a: 1, \
@@ -114,39 +109,36 @@ defmodule Protobuf.TextTest do
            }, \
            {a: 7}\
            ]\
-           }\
            """
 
     result_with_small_limit = Text.encode(input, max_line_width: 10)
 
     assert result_with_small_limit == """
-           {
+           a: 1
+           e: {
              a: 1,
-             e: {
+             b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
+           }
+           h: [
+             {
+               a: 5,
+               b: "hi"
+             },
+             {
                a: 1,
                b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
              },
-             h: [
-               {
-                 a: 5,
-                 b: "hi"
-               },
-               {
-                 a: 1,
-                 b: "HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
-               },
-               {a: 7}
-             ]
-           }\
+             {a: 7}
+           ]\
            """
   end
 
   test "encoding oneofs" do
-    assert "{a: 50}" == Text.encode(%TestMsg.Oneof{first: {:a, 50}})
+    assert "a: 50" == Text.encode(%TestMsg.Oneof{first: {:a, 50}})
   end
 
   test "encoding maps" do
-    assert ~S({l: [{key: "a", value: 1}, {key: "b", value: 2}]}) ==
+    assert ~S(l: [{key: "a", value: 1}, {key: "b", value: 2}]) ==
              Text.encode(%TestMsg.Foo{l: %{"a" => 1, "b" => 2}})
   end
 
