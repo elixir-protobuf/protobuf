1cb4cb00ab1d01b015074b560a6eaac445df86b5
v0idpwn <v0idpwn@gmail.com>
2025-04-10 12:58:33 -0300

    Bump google protos version

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Protobuf.Mixfile do
       # and make sure it's there for tests without Git submodules or anything like that.
       {:google_protobuf,
        github: "protocolbuffers/protobuf",
-       ref: "b407e8416e3893036aee5af9a12bd9b6a0e2b2e6",
+       ref: "43e1626812c1b543e56a7bec59dc09eb18248bd2",
        submodules: true,
        app: false,
        compile: false,
