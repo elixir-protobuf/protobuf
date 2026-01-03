73803b19c97e64bcd37dce068dbacb191108c601
Yordis Prieto <yordis.prieto@gmail.com>
2026-01-03 08:44:06 -0500

    feat(Any): add pack/1 function
    Add pack/1 function to Protobuf.Any module for packing protocol buffer messages into Any type. Uses the full_name/0 callback to determine type URLs.

--- test/protobuf/any_test.exs
+++ test/protobuf/any_test.exs
@@ -1,6 +1,24 @@
 defmodule Protobuf.AnyTest do
   use ExUnit.Case, async: true
 
+  doctest Protobuf.Any
+
+  describe "pack/1" do
+    test "packs a message into Any" do
+      message = %Google.Protobuf.Duration{seconds: 42}
+      assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
+      assert result.type_url == "type.googleapis.com/google.protobuf.Duration"
+      assert result.value == Google.Protobuf.Duration.encode(message)
+    end
+
+    test "packs a message into Any with a prefix" do
+      message = %My.Test.Request.SomeGroup{group_field: 42}
+      assert %Google.Protobuf.Any{} = result = Protobuf.Any.pack(message)
+      assert result.type_url == "type.googleapis.com/test.Request.SomeGroup"
+      assert result.value == My.Test.Request.SomeGroup.encode(message)
+    end
+  end
+
   describe "type_url_to_module/1" do
     test "returns the module for a valid type_url" do
       assert Protobuf.Any.type_url_to_module("type.googleapis.com/google.protobuf.Duration") ==
