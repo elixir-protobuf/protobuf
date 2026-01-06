1bb792f2b86574942df2c5f93c80258d38a03942
Yordis Prieto <yordis.prieto@gmail.com>
2026-01-06 11:58:01 -0500

    Add pack/1 function to Protobuf.Any (#426)

--- lib/protobuf/any.ex
+++ lib/protobuf/any.ex
@@ -1,10 +1,38 @@
 defmodule Protobuf.Any do
-  @moduledoc false
 
+  @moduledoc """
+  Provides functions for working with the `google.protobuf.Any` type.
+  """
+  @moduledoc since: "0.16.0"
+
+  @type_url_prefix "type.googleapis.com/"
+
+  @doc """
+  Packs a Protobuf message into a `Google.Protobuf.Any` message.
+
+  ## Example
+
+      message = MyPkg.MyMessage.new(%{field: "value"})
+      any = Protobuf.Any.pack(message)
+      #=> %Google.Protobuf.Any{
+      #=>   type_url: "type.googleapis.com/my_pkg.MyMessage",
+      #=>   value: <<...>>
+      #=> }
+
+  """
+  @spec pack(struct()) :: Google.Protobuf.Any.t()
+  def pack(%mod{} = data) do
+    %Google.Protobuf.Any{
+      type_url: "#{@type_url_prefix}#{mod.full_name()}",
+      value: mod.encode(data)
+    }
+  end
+
+  @doc false
   @spec type_url_to_module(String.t()) :: module()
   def type_url_to_module(type_url) when is_binary(type_url) do
     case type_url do
-      "type.googleapis.com/" <> package_and_message ->
+      @type_url_prefix <> package_and_message ->
         package_and_message
         |> String.split(".")
         |> Enum.map(&Macro.camelize/1)
